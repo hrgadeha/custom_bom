0b0222d9cf2f77af59df8eed204bf7c855ce8bab
bhavikpatel7023 <bhavikpatel7023@gmail.com>
2018-09-14 09:15:29 +0000

    Api and custom script

--- custom_bom/api.py
+++ custom_bom/api.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 import frappe
+import json
 
 @frappe.whitelist()
 def getBOMList(item_code):
@@ -8,5 +9,24 @@ def getBOMList(item_code):
 @frappe.whitelist()
 def getBomDetail(bom_no):
 	return frappe.get_doc("BOM",bom_no)
+	
+@frappe.whitelist()
+def saveBOM(item,items,currency):
+	doc = frappe.get_doc({
+		"docstatus":0,
+		"doctype":"BOM",
+		"name":"New BOM 1",
+		"quantity":1,
+		"is_active":1,
+		"is_default":0,
+		"rm_cost_as_per":"Price List",
+		"buying_price_list":"Standard Selling",
+		"company":frappe.defaults.get_user_default('company'),
+		"currency":currency,
+		"items":[json.loads(items)],
+		"item":item
+		})
+	doc.insert()
+		
 
 
