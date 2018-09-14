d6c146dde45df7d43f88c32398d31213b455981a
bhavikpatel7023 <bhavikpatel7023@gmail.com>
2018-09-14 08:34:32 +0000

    Api added

--- custom_bom/api.py
+++ custom_bom/api.py
@@ -1,20 +1,12 @@
 from __future__ import unicode_literals
 import frappe
-from frappe.custom.doctype.property_setter.property_setter import make_property_setter
-
-from datetime import datetime
-from frappe.utils import getdate, validate_email_add, today, add_years,add_days,format_datetime
-from erpnext.selling.doctype.sales_order.sales_order import make_sales_invoice
-from frappe.model.naming import make_autoname
-from frappe import throw, _, scrub
-import frappe.permissions
-from frappe.model.document import Document
-import json
-import collections
-# import urllib
-# import urllib2
 
 @frappe.whitelist()
-def getPrices():
-	return []
+def getBOMList(item_code):
+	return frappe.get_list("BOM",filters=[["item","=",item_code]],fields=["*"])
+
+@frappe.whitelist()
+def getBomDetail(bom_no):
+	return frappe.get_doc("BOM",bom_no)
+
 
