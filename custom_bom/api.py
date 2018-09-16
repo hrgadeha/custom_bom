b4a3713b209721c9f514822472f6536dc05320ac
bhavikpatel7023 <bhavikpatel7023@gmail.com>
2018-09-16 22:56:53 +0000

    Api and Custom script changed

--- custom_bom/api.py
+++ custom_bom/api.py
@@ -4,29 +4,37 @@ import json
 
 @frappe.whitelist()
 def getBOMList(item_code):
-	return frappe.get_list("BOM",filters=[["item","=",item_code]],fields=["*"])
+	try:
+		return frappe.get_list("BOM",filters=[["item","=",item_code]],fields=["*"])
+	except:
+		frappe.msgprint("Problem with fetching information of BOM List")
 
 @frappe.whitelist()
 def getBomDetail(bom_no):
-	return frappe.get_doc("BOM",bom_no)
-	
+	try:
+		return frappe.get_doc("BOM",bom_no)
+	except:
+		frappe.msgprint("Problem with fetching information of BOM")
 @frappe.whitelist()
 def saveBOM(item,items,currency):
-	doc = frappe.get_doc({
-		"docstatus":0,
-		"doctype":"BOM",
-		"name":"New BOM 1",
-		"quantity":1,
-		"is_active":1,
-		"is_default":0,
-		"rm_cost_as_per":"Price List",
-		"buying_price_list":"Standard Selling",
-		"company":frappe.defaults.get_user_default('company'),
-		"currency":currency,
-		"items":[json.loads(items)],
-		"item":item
-		})
-	doc.insert()
+	try:
+		doc = frappe.get_doc({
+			"docstatus":0,
+			"doctype":"BOM",
+			"name":"New BOM 1",
+			"quantity":1,
+			"is_active":1,
+			"is_default":0,
+			"rm_cost_as_per":"Price List",
+			"buying_price_list":"Standard Selling",
+			"company":frappe.defaults.get_user_default('company'),
+			"currency":currency,
+			"items":json.loads(items),
+			"item":item
+			})
+		return doc.insert()
+	except:
+		frappe.msgprint("Problem in BOM Creation")
 		
 
 
